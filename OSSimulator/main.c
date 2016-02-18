b32d41d3f7d6e124179a21298b9609c489f07024
Costas Vrahimis <cvrahimis@costass-macbook-pro.local>
2016-02-17 21:52:54 -0500

    Initial Commit

---
+++ OSSimulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OSSimulator
+//
+//  Created by Costas Vrahimis on 2/17/16.
+//  Copyright © 2016 Costas Vrahimis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
