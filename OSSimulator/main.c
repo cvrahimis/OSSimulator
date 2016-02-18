7e256c34ee90373cb87f48093edd61a2e436371b
Costas Vrahimis <cvrahimis@costass-macbook-pro.local>
2016-02-17 22:29:50 -0500

    Made a clock thread but the integer var needs to become a shared var and not a local var

--- OSSimulator/main.c
+++ OSSimulator/main.c
@@ -7,9 +7,41 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *cpuClock(void *arg){
+    int t = 0;
+    while (t < 10) {
+        t++;
+        printf("Tick: %d \n", t);
+        sleep(1);
+    }
+    
+    pthread_exit(NULL);
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int rc;
+    pthread_t clockThread;
+    
+    rc = pthread_create(&clockThread, NULL, cpuClock, NULL);
+    if(rc)
+    {
+        printf("ERROR. Return code from thread %d\n", rc);
+        exit(-1);
+    }
+    
+    rc = pthread_join(clockThread, NULL);
+    if(rc)
+    {
+        printf("Error, return code from thread_join() is %d\n", rc);
+        exit(-1);
+    }
+    printf("main completed join with thread clock thread \n");
+    
+
     return 0;
 }
